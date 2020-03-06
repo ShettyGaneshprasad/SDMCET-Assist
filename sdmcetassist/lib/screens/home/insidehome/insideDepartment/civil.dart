c45bcbed925cae372bf6e2c67977edd0c1c8f97c
Shubhamkumar6458 <shubhamkumar6458@gmail.com>
2020-03-06 16:52:52 +0530

    Update civil.dart

--- sdmcetassist/lib/screens/home/insidehome/insideDepartment/civil.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideDepartment/civil.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:sdmcetassist/shared/loading.dart';
+
+import 'fullscreen_image.dart';
 
 class Civil extends StatelessWidget {
   @override
@@ -67,8 +74,8 @@ class Civil extends StatelessWidget {
               margin: EdgeInsets.all(8.0),
               child: InkWell(
                 onTap: () {
-                  //                Navigator.push(context,
-//                      MaterialPageRoute(builder: (context) => Department()));
+                                 Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TimeTable()));
                 },
                 splashColor: Colors.lightBlueAccent,
                 child: Center(
@@ -113,7 +120,7 @@ class Civil extends StatelessWidget {
     );
   }
 }
-
+//Start of About Class
 class CivilAboutp1 extends StatelessWidget {
   const CivilAboutp1({Key key}) : super(key: key);
 
@@ -164,3 +171,78 @@ class CivilAboutp1 extends StatelessWidget {
     );
   }
 }
+//End of About Class
+//class time table
+
+class TimeTable extends StatefulWidget {
+  _TimeTable createState() => new _TimeTable();
+}
+
+class _TimeTable extends State<TimeTable> {
+  StreamSubscription<QuerySnapshot> subscription;
+  List<DocumentSnapshot> timeTable;
+
+  final CollectionReference collectionReference =
+      Firestore.instance.collection("ISETimeTable");
+
+  @override
+  void initState() {
+    super.initState();
+    subscription = collectionReference.snapshots().listen((datasnapshot) {
+      setState(() {
+        timeTable = datasnapshot.documents;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[50],
+        appBar: AppBar(
+          title: Text(
+            'Civil Department', /*style:TextStyle(color:Colors.black)*/
+          ),
+          backgroundColor: Colors.blue[300],
+          elevation: 10.0,
+        ),
+        body: timeTable != null
+            ? new StaggeredGridView.countBuilder(
+                padding: const EdgeInsets.all(9.0),
+                crossAxisCount: 1,
+                itemCount: timeTable.length,
+                itemBuilder: (context, i) {
+                  String imgPath = timeTable[i].data['url'];
+                  return new Material(
+                      elevation: 10.0,
+                      borderRadius:
+                          new BorderRadius.all(new Radius.circular(50.0)),
+                      child: new InkWell(
+                        onTap: () => Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                                builder: (context) =>
+                                    new FullScreenImagePage(imgPath))),
+                        child: Hero(
+                          tag: imgPath,
+                          child: new FadeInImage(
+                            placeholder: new AssetImage("assets/sdmlogo.jpg"),
+                            image: new NetworkImage(imgPath),
+                          ),
+                        ),
+                      ));
+                },
+                staggeredTileBuilder: (i) => new StaggeredTile.count(1, 1),
+                mainAxisSpacing: 2.0,
+                crossAxisSpacing: 2.0,
+              )
+            : new Loading());
+  }
+}
+//end of class TimeTable
