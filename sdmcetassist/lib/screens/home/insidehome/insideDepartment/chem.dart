fd022446a42ac9d59b682037175363fc3cc036ae
Shubhamkumar6458 <shubhamkumar6458@gmail.com>
2020-03-06 16:52:34 +0530

    Update chem.dart

--- sdmcetassist/lib/screens/home/insidehome/insideDepartment/chem.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideDepartment/chem.dart
@@ -5,17 +5,169 @@ import 'dart:async';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:sdmcetassist/screens/home/insidehome/insideDepartment/fullscreen_image.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:sdmcetassist/shared/loading.dart';
 
-class Chemical extends StatefulWidget {
-  _ChemicalState createState() => new _ChemicalState();
+class Chemical extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[50],
+      appBar: AppBar(
+        title: Text(
+          'Chemical Department', /*style:TextStyle(color:Colors.black)*/
+        ),
+        backgroundColor: Colors.blue[300],
+        elevation: 10.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ChemAboutp1()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.account_balance,
+                        size: 50.0,
+                      ),
+                      Text("About", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                 // Navigator.push(
+                    //  context,
+                   //   MaterialPageRoute(
+                    //      builder: (context) => FacultyDetails()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
+                        size: 50.0,
+                      ),
+                      Text("Faculty", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TimeTable()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.access_time,
+                        size: 50.0,
+                      ),
+                      Text("Time Table", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  //        Navigator.push(context,
+                  //              MaterialPageRoute(builder: (context) => Contacts()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.warning,
+                        size: 50.0,
+                      ),
+                      Text("Notice", style: new TextStyle(fontSize: 17.0))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+//Start of About Class
+class ChemAboutp1 extends StatelessWidget {
+  const ChemAboutp1({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[50],
+      appBar: AppBar(
+        title: Text(
+          'Chemical Department', /*style:TextStyle(color:Colors.black)*/
+        ),
+        backgroundColor: Colors.blue[300],
+        elevation: 10.0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Wrap(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "Chemical Engineering Department at the SDMCET, Dharwad was established in the year 1995. The department is recognized by AICTE and affiliated to Visvesvaraya Technological University, Belagavi. The program gained an Autonomous Status since 2007 and accredited thrice (Recent: 2018-2021 under Tier-I category). The sanctioned intake of the department is 30 for the Undergraduate program i.e. B.E. in Chemical Engineering. The department takes pride in it’s past achievements and has the dream supported by action plans to make it as one of the best Chemical Engineering Department to develop the proficient engineers to meet the industrial and societal needs with human values.",
+                  textAlign: TextAlign.justify,
+                  softWrap: true,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+//End of About Class
+//class time table
+
+class TimeTable extends StatefulWidget {
+  _TimeTable createState() => new _TimeTable();
 }
 
-class _ChemicalState extends State<Chemical> {
+class _TimeTable extends State<TimeTable> {
   StreamSubscription<QuerySnapshot> subscription;
   List<DocumentSnapshot> timeTable;
 
   final CollectionReference collectionReference =
-      Firestore.instance.collection("timeTable");
+      Firestore.instance.collection("ISETimeTable");
 
   @override
   void initState() {
@@ -70,45 +222,11 @@ class _ChemicalState extends State<Chemical> {
                         ),
                       ));
                 },
-                staggeredTileBuilder: (i) => new StaggeredTile.count(4, 4),
+                staggeredTileBuilder: (i) => new StaggeredTile.count(1, 1),
                 mainAxisSpacing: 2.0,
                 crossAxisSpacing: 2.0,
               )
-            : new LinearProgressIndicator());
+            : new Loading());
   }
 }
-
-class ChemAboutp1 extends StatelessWidget {
-  const ChemAboutp1({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text(
-          'Chemical Department', /*style:TextStyle(color:Colors.black)*/
-        ),
-        backgroundColor: Colors.blue[300],
-        elevation: 10.0,
-      ),
-      body: Column(
-        children: <Widget>[
-          Wrap(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Text(
-                  "Chemical Engineering Department at the SDMCET, Dharwad was established in the year 1995. The department is recognized by AICTE and affiliated to Visvesvaraya Technological University, Belagavi. The program gained an Autonomous Status since 2007 and accredited thrice (Recent: 2018-2021 under Tier-I category). The sanctioned intake of the department is 30 for the Undergraduate program i.e. B.E. in Chemical Engineering. The department takes pride in it’s past achievements and has the dream supported by action plans to make it as one of the best Chemical Engineering Department to develop the proficient engineers to meet the industrial and societal needs with human values.",
-                  textAlign: TextAlign.justify,
-                  softWrap: true,
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
+//end of class TimeTable
