117e52f2f4030fe435f89758378a9b998b0f457b
ganeshShetty98 <gs00619@gmail.com>
2020-03-07 17:39:07 +0530

    Create visionMission.dart

---
+++ sdmcetassist/lib/screens/home/insidehome/insideAbout/visionMission.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Visionmission extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("vision mission"),
+    );
+  }
+}
