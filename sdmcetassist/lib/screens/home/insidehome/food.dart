2dfce508774d61164c33ae1332174ebf1a547b57
Shubhamkumar6458 <shubhamkumar6458@gmail.com>
2020-03-08 01:44:07 +0530

    Update food.dart

--- sdmcetassist/lib/screens/home/insidehome/food.dart
+++ sdmcetassist/lib/screens/home/insidehome/food.dart
@@ -13,6 +13,126 @@ class Food extends StatelessWidget {
         backgroundColor: Colors.blue[300],
         elevation: 10.0,
       ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          // crossAxisCount: 3,
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+               //   Navigator.push(context,
+                 //     MaterialPageRoute(builder: (context) => firstYearUg()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.restaurant,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "Suruchi",
+                        style: new TextStyle(fontSize: 30.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+               //   Navigator.push(context,
+                 //     MaterialPageRoute(builder: (context) => firstYearUg()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.restaurant,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "Santrupti Mess",
+                        style: new TextStyle(fontSize: 30.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+               //   Navigator.push(context,
+                 //     MaterialPageRoute(builder: (context) => firstYearUg()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.restaurant,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "College Cafeteria",
+                        style: new TextStyle(fontSize: 30.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+               //   Navigator.push(context,
+                 //     MaterialPageRoute(builder: (context) => firstYearUg()));
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.restaurant,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "College Bakery",
+                        style: new TextStyle(fontSize: 30.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            ],
+        ),
+      ),
     );
   }
 }
