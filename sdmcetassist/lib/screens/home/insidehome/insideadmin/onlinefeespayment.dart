470e1bfca5ea3582568dbc6dc0da9851167e8714
ganeshShetty98 <gs00619@gmail.com>
2020-03-07 17:39:14 +0530

    Create onlinefeespayment.dart

---
+++ sdmcetassist/lib/screens/home/insidehome/insideadmin/onlinefeespayment.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OnlineFeesPayment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("sdmcet"),
+    );
+  }
+}
