979b11a74b192d315c531abf2cbc8a16b43e3bc5
ganeshShetty98 <gs00619@gmail.com>
2020-03-07 17:38:50 +0530

    Update administration.dart

--- sdmcetassist/lib/screens/home/insidehome/administration.dart
+++ sdmcetassist/lib/screens/home/insidehome/administration.dart
@@ -1,18 +1,67 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'insideadmin/admission.dart';
+import 'insideadmin/certificateFromAdmin.dart';
+import 'insideadmin/onlinefeespayment.dart';
 
-class Administration extends StatelessWidget {
+class Administration extends StatefulWidget {
+  MyTabsState createState() => new MyTabsState();
+}
+
+class MyTabsState extends State<Administration>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text(
-          'Administration', /*style:TextStyle(color:Colors.black)*/
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue[300],
+            title: Text("Administration"),
+            bottom: menu(),
+          ),
+          body: TabBarView(children: [
+            Admission(),
+            OnlineFeesPayment(),
+            CertificateFromAdmin(),
+          ]),
         ),
-        backgroundColor: Colors.blue[300],
-        elevation: 10.0,
       ),
+    );
+  }
+
+  Widget menu() {
+    return TabBar(
+      tabs: [
+        Tab(
+          text: "Admission",
+          icon: Icon(Icons.account_balance),
+        ),
+        Tab(
+          text: "Online Fees Payment",
+          icon: Icon(Icons.monetization_on),
+        ),
+        Tab(
+          text: "Certificate from Admin",
+          icon: Icon(Icons.assignment),
+        ),
+      ],
     );
   }
 }
