09bcff0862958f146a32c926a2e06071c3db601e
ganeshShetty98 <gs00619@gmail.com>
2020-03-08 18:06:38 +0530

    Update admission.dart

--- sdmcetassist/lib/screens/home/insidehome/insideadmin/admission.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideadmin/admission.dart
@@ -5,7 +5,26 @@ class Admission extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("sdmcet"),
-    );
+        padding: EdgeInsets.all(20.0),
+        child: ListView(children: <Widget>[
+          Text(
+            "Overview",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nSDMCET a premier institute in India accepts admissions through 3 selection processes for all branches of undergraduate courses: Viz. COMMON Entrance TEST(CET), COMEDK and MANAGEMENT quota\n",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+        ]));
   }
 }
