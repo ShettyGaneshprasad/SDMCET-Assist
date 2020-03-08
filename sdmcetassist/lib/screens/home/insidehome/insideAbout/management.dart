b909a320698304de47245c3c7b780c3f03577dc2
ganeshShetty98 <gs00619@gmail.com>
2020-03-08 11:26:08 +0530

    Update management.dart

--- sdmcetassist/lib/screens/home/insidehome/insideAbout/management.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideAbout/management.dart
@@ -4,8 +4,55 @@ import 'package:flutter/material.dart';
 class Management extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("management"),
-    );
+    {
+      return Container(
+        child: ListView(
+          children: <Widget>[
+            Image.network(
+              'https://firebasestorage.googleapis.com/v0/b/sdmcet-assist.appspot.com/o/CollegePic%2Fabout-veerendra-heggade.png?alt=media&token=83596196-929c-4bcc-a519-bcbca57af05d',
+            ),
+            Text(
+              "Dr D Veerendra Heggade",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+                fontSize: 40,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+            Text(
+              "\nDharmaadhikari, Reformer, Educationist, Philosopher and Philanthropist, Dr. D.Veerendra Heggade is a multidimensional individual. Years of path-breaking contribution to social, economical and cultural development of the society, is what truly defines his unyielding service to the nation. From having converted small towns into modern educational hubs, redefined the very role of a temple from being a place of worship to a place of service, revived ancient sciences to further enhance modern lifestyles, and most importantly, having shown that one individual has the power to fuel and ignite the status quo condition of a nation. This is exactly why in 2015, Dr. D. Veerendra Heggade was awarded the second highest award a civilian can receive in India, the Padma Vibhushan. As the President of SDM Educational Society ,He has established over 50 institutions and has been quite instrumental in uplifting the educational standards throughout Karnataka.\n",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+            Image.network(
+              'https://firebasestorage.googleapis.com/v0/b/sdmcet-assist.appspot.com/o/CollegePic%2Fabout-sdme-society.png?alt=media&token=66f26d01-e12a-4e7f-a504-8fdb5cb40e23',
+            ),
+            Text(
+              "SDME Society",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+                fontSize: 40,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+            Text(
+              "\nSDM Educational Society (R) is a premier non-profit educational organization, functioning under the sacred aegis of Shree Kshetra Dharmasthala. SDME Society spearheads the activities of over 50 premier academic and professional institutions spread across Karnataka. Establishments that represent the best of traditional expertise and contemporary excellence form the base for studies in subjects ranging from Medicine, Engineering, Law, Social Sciences, Management, Naturopathy and Ayurveda.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
