e48ed38c72173cf3413c43ebc842709cb5d0129d
ganeshShetty98 <gs00619@gmail.com>
2020-03-08 17:39:35 +0530

    Update onlinefeespayment.dart

--- sdmcetassist/lib/screens/home/insidehome/insideadmin/onlinefeespayment.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideadmin/onlinefeespayment.dart
@@ -5,7 +5,37 @@ class OnlineFeesPayment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("sdmcet"),
-    );
+        child: ListView(
+      children: <Widget>[
+        Text(
+          "\nSteps to be followed for \nonline fee payment.",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+            fontSize: 30,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+        Text(
+          "\n   1.Enter your USN in the space for user ID\n   2.Enter your date of birth as the password\n   3.Then click on the Login button.\n   4.The details will be displayed.\n   5.Click on – I agree and Initiate payment.\n   6.Select the mode of paymen.\n",
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 20,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+        Text(
+          "\nNote:",
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.red,
+            fontSize: 30,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+      ],
+    ));
   }
 }
