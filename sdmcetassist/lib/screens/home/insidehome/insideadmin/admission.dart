0f94722c050e17e15fbf4fd000f708faa451f96a
ganeshShetty98 <gs00619@gmail.com>
2020-03-09 16:13:53 +0530

    Update admission.dart

--- sdmcetassist/lib/screens/home/insidehome/insideadmin/admission.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideadmin/admission.dart
@@ -723,10 +723,8 @@ class LateralEntry extends StatelessWidget {
         padding: EdgeInsets.all(20.0),
         child: ListView(
           children: <Widget>[
-            Image.network(
-                'https://firebasestorage.googleapis.com/v0/b/sdmcet-assist.appspot.com/o/CollegePic%2Fkcet.jpg?alt=media&token=3a77e751-9fa9-46d7-a5c8-c6a5e54ba5e2'),
             Text(
-              "ELIGIBILITY FOR GOVERNMENT SEATS",
+              "LATERAL ENTRY to III SEMESTER for Diploma / B.Sc. students",
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -736,23 +734,7 @@ class LateralEntry extends StatelessWidget {
               ),
             ),
             Text(
-              "\nThe qualifying examinations prescribed for admission for B.E. is second PUC or 10+2 exam or equivalent examination recognized by State / Central Government/ University with English as a compulsory subject.",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                fontSize: 20,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-            Text(
-              "\nThe General Merit candidates should have passed 2 nd PUC or 10+2 examination with a minimum aggregate of 45% marks (40% in respect of SC, ST and OBC candidates of Karnataka State) in Physics and Mathematics along with Chemistry or Bio Technology or Biology or Computer Science or Electronics as one of the optional subjects. English must be a subject of study. However, educational qualifications/eligibility criteria prescribed by the competent authority in regulations/guidelines issued from time to time will be applicable in deciding the eligibility.",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                fontSize: 20,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-            Text(
-              "\nQualification:-",
+              "\nELIGIBILITY CRITERIA:-",
               textAlign: TextAlign.left,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -762,7 +744,7 @@ class LateralEntry extends StatelessWidget {
               ),
             ),
             Text(
-              "Pass in 10+2 or equivalent with Physics, Mathematics as compulsory subjects along with Chemistry or Biotechnology or Biology or Computer science or Electronics with a minimum of 45% marks taken together in Physics, Mathematics and any one of the optional subjects.",
+              "\nNo candidate shall be eligible for admission to Government Seats unless he is a citizen of India and satisfies eligibility conditions. He/she should have appeared for DCET examination.",
               textAlign: TextAlign.justify,
               style: TextStyle(
                 fontSize: 20,
@@ -770,67 +752,40 @@ class LateralEntry extends StatelessWidget {
               ),
             ),
             Text(
-              "\nELIGIBILITY CLAUSES:-",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                fontSize: 30,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-            Text(
-              "The candidate should have studied and passed in one or more Government or Government recognized educational institutions located in the State of Karnataka for a minimum period of SEVEN academic years commencing from 1st standard to 2nd PUC / 12th standard as on 1st July of the year in which the Entrance Test is held.Eligibility/Admissions/allotments made to & by the colleges shall be subject to approval by AICTE/Government/University.",
+              "\nEach condition is specified as a clause and is individually referred by a code. The documents to be produced during verification of documents in support of the claim for each of the Clauses.",
               textAlign: TextAlign.justify,
               style: TextStyle(
                 fontSize: 20,
                 fontStyle: FontStyle.normal,
               ),
             ),
+
             Text(
-              "\nDocuments to be produced:-",
-              textAlign: TextAlign.left,
+              "\nThe candidates who do not satisfy any of the Clauses / Eligibility Criteria and the candidates who do not produce the prescribed documents in original will not be eligible for admission to professional courses under Government quota seats.",
+              textAlign: TextAlign.justify,
               style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                fontSize: 30,
+                fontSize: 20,
                 fontStyle: FontStyle.normal,
               ),
             ),
+
             Text(
-              "\n1. SSLC or 10 th Marks Card\n2. 2nd PUC / 12th standard Marks Card of the candidate.\n3. Candidate’s Study Certificate.\n4. If claiming Rural quota: – Rural Study Certificate from 1st to 10th std for Ten Complete Years.\n5. If claiming Kannada Medium quota: Kannada Medium Study Certificate from 1st to 10th std for Ten Complete Years.\n6. If claiming reservation benefits: Caste / Caste Income Certificate issued by Concerned Tahsildar- For SC / ST in Form-D, Category-I in form E and 2A, 2B, 3A and 3B in Form F.\n7. Transfer Certificate from previous Educational Institute.\n",
+              "\nOther than the certificate mentioned in clauses, if a candidate claiming seat under Article 371 (j) Hyderabad-Karnataka region reservation, candidate should produce eligibility certificate from the concerned Assistant Commissioner in Annexure-A as per the format.",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+
+            Text(
+              "\nCandidates should have passed any diploma or equivalent examination in Karnataka and ovtained an aggregate minimum of 45% marks( 40% marks in case of SC,ST and Backward Classes of Karnataka Candidates. Candidates with B.Sc degree are also eligible.",
               textAlign: TextAlign.justify,
               style: TextStyle(
                 fontSize: 20,
                 fontStyle: FontStyle.normal,
               ),
             ), //enter below
-            Card(
-              elevation: 5,
-              margin: EdgeInsets.all(13.0),
-              child: InkWell(
-                onTap: () {
-                  launch('http://kea.kar.nic.in/');
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        "\nTo Get More Info Click Here\n",
-                        style: new TextStyle(
-                            fontSize: 20.0,
-                            fontStyle: FontStyle.italic,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red),
-                        textAlign: TextAlign.center,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
           ],
         ),
       ),
