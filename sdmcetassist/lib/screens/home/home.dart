fbd7a26c3d24605a35e4dc0371c7420b64258a18
ganeshShetty98 <gs00619@gmail.com>
2020-03-10 22:43:24 +0530

    Update home.dart

--- sdmcetassist/lib/screens/home/home.dart
+++ sdmcetassist/lib/screens/home/home.dart
@@ -7,6 +7,7 @@ import 'package:sdmcetassist/screens/home/insidehome/navigation.dart';
 import 'package:sdmcetassist/screens/home/insidehome/transport.dart';
 import 'insidehome/department.dart';
 import 'package:sdmcetassist/services/auth.dart';
+import 'package:simple_slider/simple_slider.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
@@ -55,170 +56,195 @@ class Home extends StatelessWidget {
         ),
       ),
       body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: GridView.count(
-          crossAxisCount: 3,
-          children: <Widget>[
-            Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => About()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.account_balance,
-                        size: 50.0,
+          padding: EdgeInsets.all(20.0),
+          child: PageView(children: <Widget>[
+            GridView.count(
+              crossAxisCount: 3,
+              children: <Widget>[
+                Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => About()));
+                    },
+                    splashColor: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.account_balance,
+                            size: 50.0,
+                          ),
+                          Text("About", style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
-                      Text("About", style: new TextStyle(fontSize: 17.0))
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Administration()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.collections_bookmark,
-                        size: 50.0,
+                Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Administration()));
+                    },
+                    splashColor: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.collections_bookmark,
+                            size: 50.0,
+                          ),
+                          Text("Admin", style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
-                      Text("Admin", style: new TextStyle(fontSize: 17.0))
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Department()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.school,
-                        size: 50.0,
+                Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Department()));
+                    },
+                    splashColor: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.school,
+                            size: 50.0,
+                          ),
+                          Text("Academics",
+                              style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
-                      Text("Academics", style: new TextStyle(fontSize: 17.0))
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Contacts()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.contact_phone,
-                        size: 50.0,
+                Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Contacts()));
+                    },
+                    splashColor: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.contact_phone,
+                            size: 50.0,
+                          ),
+                          Text("Contacts", style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
-                      Text("Contacts", style: new TextStyle(fontSize: 17.0))
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => Food()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.restaurant,
-                        //color: Colors.amberAccent,
-                        size: 50.0,
+                Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Food()));
+                    },
+                    splashColor: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.restaurant,
+                            //color: Colors.amberAccent,
+                            size: 50.0,
+                          ),
+                          Text("Food", style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
-                      Text("Food", style: new TextStyle(fontSize: 17.0))
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Navigation()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.add_location,
-                        size: 50.0,
+                Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Navigation()));
+                    },
+                    splashColor: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.add_location,
+                            size: 50.0,
+                          ),
+                          Text("Navigation",
+                              style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
-                      Text("Navigation", style: new TextStyle(fontSize: 17.0))
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Transport()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.departure_board,
-                        size: 50.0,
+                Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Transport()));
+                    },
+                    splashColor: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.departure_board,
+                            size: 50.0,
+                          ),
+                          Text("Bus", style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
-                      Text("Bus", style: new TextStyle(fontSize: 17.0))
-                    ],
+                    ),
                   ),
                 ),
-              ),
+                Container(
+                  child: PageView(
+                    children: <Widget>[
+                      Text("sedvsfb"),
+                      Text("sedvsfb"),
+                      Text("sedvsfb"),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
-      ),
+            Container(
+              child: PageView(
+                children: <Widget>[
+                  Text("sedvsfb"),
+                  Text("sedvsfb"),
+                  Text("sedvsfb"),
+                ],
+              ),
+            )
+          ])),
     );
   }
 }
