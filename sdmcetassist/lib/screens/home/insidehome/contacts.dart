92a14e833b51f7b8ef85a723b184ee55706d8f80
ganeshShetty98 <gs00619@gmail.com>
2020-03-10 16:19:28 +0530

    Update contacts.dart

--- sdmcetassist/lib/screens/home/insidehome/contacts.dart
+++ sdmcetassist/lib/screens/home/insidehome/contacts.dart
@@ -1,18 +1,88 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sdmcetassist/screens/home/insidehome/insideContact/administrationContact.dart';
+import 'package:sdmcetassist/screens/home/insidehome/insideContact/hodContact.dart';
 
-class Contacts extends StatelessWidget {
+class Contacts extends StatefulWidget {
+  MyTabsState createState() => new MyTabsState();
+}
+
+class MyTabsState extends State<Contacts> with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text(
-          'Contacts', /*style:TextStyle(color:Colors.black)*/
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue[300],
+            title: Text("About"),
+            bottom: menu(),
+          ),
+          body: TabBarView(
+            children: [
+              AdministrationContact(),
+              HodContact(),
+              HodContact(),
+              HodContact()
+            ],
+          ),
         ),
-        backgroundColor: Colors.blue[300],
-        elevation: 10.0,
       ),
+    );
+  }
+
+  Widget menu() {
+    return TabBar(
+      tabs: [
+        Tab(
+          child: Flexible(
+              child: Text(
+            "SDMCET",
+            textAlign: TextAlign.center,
+          )),
+          icon: Icon(Icons.account_balance),
+        ),
+        Tab(
+          child: Flexible(
+              child: Text(
+            "Management",
+            textAlign: TextAlign.center,
+          )),
+          icon: Icon(Icons.assignment),
+        ),
+        Tab(
+          child: Flexible(
+              child: Text(
+            "Deans",
+            textAlign: TextAlign.center,
+          )),
+          icon: Icon(Icons.people),
+        ),
+        Tab(
+          child: Flexible(
+              child: Text(
+            "Vission Mission",
+            textAlign: TextAlign.center,
+          )),
+          icon: Icon(Icons.device_hub),
+        ),
+      ],
     );
   }
 }
