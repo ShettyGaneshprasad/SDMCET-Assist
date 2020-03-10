e4a2d5d4ad7a9443efc1c7f295aa0194452397f7
ganeshShetty98 <gs00619@gmail.com>
2020-03-10 18:54:51 +0530

    Update hodContact.dart

--- sdmcetassist/lib/screens/home/insidehome/insideContact/hodContact.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideContact/hodContact.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HodContact extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.lightBlue[100],
       child: ListView(
         children: <Widget>[
           Text(
@@ -17,193 +19,372 @@ class HodContact extends StatelessWidget {
                 decoration: TextDecoration.underline),
           ),
           Text(
-            "\nComputer Science Dept.",
+            "( Tap on number to call directly )",
             textAlign: TextAlign.center,
             style: TextStyle(
               fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
+              color: Colors.red,
+              fontSize: 20,
             ),
           ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(13.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:9448915301");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Computer Science Dept.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:9448915301",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "\nInformation Science  Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:9480750607");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Information Science  Dept.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:9480750607",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "Phone no: 9480750607",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:7900129925");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Mechanical Dept.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:7900129925",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "\nMechanical Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:9845511315");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "E&C Dept.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:9845511315",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:7900129925");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "E&E Dept.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:7900129925",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "\nE&C Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:7900129925");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Chemical Dept.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:7900129925",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:7900129925");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Civil Dept.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:7900129925",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "\nE&E Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:7900129925");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Dept. of Mathematics",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:7900129925",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:7900129925");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Dept. of Chemistry",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:7900129925",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          Text(
-            "\nChemical Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nCivil Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nDept. of Mathematics",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nDept. of Chemistry",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nDept. Physics",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(10.0),
+            child: InkWell(
+              onTap: () {
+                launch("tel:7900129925");
+              },
+              splashColor: Colors.lightBlueAccent,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Dept. Physics",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      "Phone no:7900129925",
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ],
