ac9edeff2d8e6f5dac201f54eb2c6088381618d3
ganeshShetty98 <gs00619@gmail.com>
2020-03-10 16:57:36 +0530

    Update hodContact.dart

--- sdmcetassist/lib/screens/home/insidehome/insideContact/hodContact.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideContact/hodContact.dart
@@ -11,14 +11,184 @@ class HodContact extends StatelessWidget {
             "HOD's contact",
             textAlign: TextAlign.center,
             style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 50,
+                decoration: TextDecoration.underline),
+          ),
+          Text(
+            "\nComputer Science Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
               fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 50,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
               fontStyle: FontStyle.normal,
             ),
           ),
           Text(
-            "\nComputer Science Dept.",
+            "\nInformation Science  Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nMechanical Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nE&C Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nE&E Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nChemical Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nCivil Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nDept. of Mathematics",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nDept. of Chemistry",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nDept. Physics",
             textAlign: TextAlign.center,
             style: TextStyle(
               fontWeight: FontWeight.bold,
@@ -33,322 +203,6 @@ class HodContact extends StatelessWidget {
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "\nComputer Science Dept.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "Phone no: 7900129925\n\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Quality Policy",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          Text(
-            "\n“In its quest to be a role model institution, committed to meet or exceed the utmost interest of all the stake holders.”\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              // /color: Colors.blue,
-              fontSize: 25,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Core Values",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-              fontSize: 40,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "• Competency\n• Commitment\n• Equity\n• Team work\n• Trust\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "66000+",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              fontSize: 40,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Printed journals, online publications in partnership with Pearson\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "15000+",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              fontSize: 40,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Graduated alumni since 1983\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "1500+",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              fontSize: 40,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Students residing on campus.\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "2",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              fontSize: 40,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Printed journals, online publications in partnership with Pearson\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "10+",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              fontSize: 40,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Memorandums signed with various organizations.\n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Following UG Programs are Accredited by NBA Under Tier-1",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              fontSize: 40,
-              fontStyle: FontStyle.normal,
-            ),
-          ),
-          Text(
-            "Mechanical Engineering \nElectrical & Electronics Engineering \nChemical Engineering \nComputer Science and Engineering\nElectronics and Communication Engineering \nCivil Engineering \nInformation Science and Engineering \n\n SDMCET has been accredited twice by NBA in Past \n",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
               fontStyle: FontStyle.normal,
             ),
           ),
