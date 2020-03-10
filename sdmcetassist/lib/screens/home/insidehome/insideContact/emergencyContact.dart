09c3973c73f6c56815362056bdb4c058472a6295
ganeshShetty98 <gs00619@gmail.com>
2020-03-10 17:02:37 +0530

    Update emergencyContact.dart

--- sdmcetassist/lib/screens/home/insidehome/insideContact/emergencyContact.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideContact/emergencyContact.dart
@@ -6,7 +6,207 @@ class EmergencyContact extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListView(
-        children: <Widget>[],
+        children: <Widget>[
+          Text(
+            "HOD's contact",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 50,
+                decoration: TextDecoration.underline),
+          ),
+          Text(
+            "\nComputer Science Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nInformation Science  Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nMechanical Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nE&C Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nE&E Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nChemical Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nCivil Dept.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nDept. of Mathematics",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nDept. of Chemistry",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          Text(
+            "\nDept. Physics",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Phone no: 7900129925\n\n",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+        ],
       ),
     );
   }
