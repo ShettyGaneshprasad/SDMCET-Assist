3156868f2d01cbb2f51c1ad5d52d7c9a5399859f
Shubhamkumar6458 <shubhamkumar6458@gmail.com>
2020-03-12 15:13:27 +0530

    Update firstYearUg.dart

--- sdmcetassist/lib/screens/home/insidehome/insideDepartment/firstYearUg.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideDepartment/firstYearUg.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
+
 import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
