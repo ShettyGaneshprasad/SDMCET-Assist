d2c5cdb7ced0c01bd5d101d7166bb3114a0aa0aa
ganeshShetty98 <gs00619@gmail.com>
2020-03-22 21:20:01 +0530

    Update chinfye.dart

--- sdmcetassist/lib/screens/home/insidehome/insideAboutDeveloper/chinfye.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideAboutDeveloper/chinfye.dart
@@ -1,22 +1,151 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/fa_icon.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Chinfye extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text(
-          'Chinmayee Joshi', /*style:TextStyle(color:Colors.black)*/
-        ),
-        backgroundColor: Colors.blue[300],
-        elevation: 10.0,
-      ),
       body: ListView(
         children: <Widget>[
           Image.network(
-              'https://firebasestorage.googleapis.com/v0/b/sdmcet-assist.appspot.com/o/Developer%2Fganeshshetty.jpg?alt=media&token=16a60483-afef-4e2b-a693-89ebfc7e9b4f')
+              'https://firebasestorage.googleapis.com/v0/b/sdmcet-assist.appspot.com/o/Developer%2Fganeshshetty.jpg?alt=media&token=16a60483-afef-4e2b-a693-89ebfc7e9b4f'),
+          Text(
+            "Chinmayee Joshi",
+            style: new TextStyle(
+                fontSize: 30.0,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.w600,
+                color: Colors.black),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            "Computer Science Department, SDMCET",
+            style: new TextStyle(
+                fontSize: 20.0,
+                fontStyle: FontStyle.normal,
+                color: Colors.black),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            "Ph :7975854141",
+            style: new TextStyle(
+                fontSize: 20.0,
+                fontStyle: FontStyle.normal,
+                color: Colors.black),
+            textAlign: TextAlign.center,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                    elevation: 10,
+                    margin: EdgeInsets.all(20.0),
+                    child: InkWell(
+                      onTap: () {
+                        launch('https://github.com/chinmayee143');
+                      },
+                      splashColor: Colors.lightBlueAccent,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          FaIcon(
+                            FontAwesomeIcons.github,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 10,
+                    margin: EdgeInsets.all(20.0),
+                    child: InkWell(
+                      onTap: () {
+                        launch(
+                            'https://www.linkedin.com/in/shetty-ganeshprasad-955599184/');
+                      },
+                      splashColor: Colors.lightBlueAccent,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          FaIcon(
+                            FontAwesomeIcons.linkedin,
+                            color: Colors.blue,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 10,
+                    margin: EdgeInsets.all(20.0),
+                    child: InkWell(
+                      onTap: () {
+                        launch('https://www.instagram.com/_g4nesh.shetty_/');
+                      },
+                      splashColor: Colors.lightBlueAccent,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          FaIcon(
+                            FontAwesomeIcons.instagram,
+                            color: Colors.purple,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 10,
+                    margin: EdgeInsets.all(20.0),
+                    child: InkWell(
+                      onTap: () {
+                        launch(
+                            'https://www.facebook.com/ganeshshetty.santhosh');
+                      },
+                      splashColor: Colors.lightBlueAccent,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          FaIcon(
+                            FontAwesomeIcons.facebookSquare,
+                            color: Colors.blue,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 10,
+                    margin: EdgeInsets.all(20.0),
+                    child: InkWell(
+                      onTap: () {
+                        launch('https://wa.me/+917975854141');
+                      },
+                      splashColor: Colors.lightBlueAccent,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          FaIcon(
+                            FontAwesomeIcons.whatsappSquare,
+                            color: Colors.green,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
