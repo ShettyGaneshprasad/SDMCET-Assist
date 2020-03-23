839a6216a34d056a1a06b728dcd52a1848db32cf
ganeshShetty98 <gs00619@gmail.com>
2020-03-23 13:24:21 +0530

    Create MorningScreen.dart

---
+++ sdmcetassist/lib/screens/home/insidehome/insideTransport/MorningScreen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'PlaceScreen.dart';
+import 'PlaceScreen9.dart';
+
+class MorningScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Morning session'),
+        backgroundColor: Colors.blue[300],
+        elevation: 10.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PlaceScreen()),
+                  );
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.access_time,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "8 clock",
+                        style: new TextStyle(fontSize: 25.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PlaceScreen9()),
+                  );
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.access_time,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "9 clock",
+                        style: new TextStyle(fontSize: 25.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
