e41e0b2963f7cc6310cfd9019dbad573b0f772af
ganeshShetty98 <gs00619@gmail.com>
2020-03-24 13:24:14 +0530

    Update sign_in.dart

--- sdmcetassist/lib/screens/authenticate/sign_in.dart
+++ sdmcetassist/lib/screens/authenticate/sign_in.dart
@@ -44,7 +44,7 @@ class _SignInState extends State<SignIn> {
               padding: EdgeInsets.symmetric(vertical: 80.0, horizontal: 30.0),
               child: Form(
                 key: _formKey,
-                child: Column(
+                child: Wrap(
                   children: <Widget>[
                     SizedBox(height: 1.0),
                     TextFormField(
