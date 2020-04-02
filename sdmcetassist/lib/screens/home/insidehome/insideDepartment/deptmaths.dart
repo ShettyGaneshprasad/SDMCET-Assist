8924e5c9fcab6898d8471d8144090ba499f2118a
ganeshShetty98 <gs00619@gmail.com>
2020-04-02 22:35:24 +0530

    Update deptmaths.dart

--- sdmcetassist/lib/screens/home/insidehome/insideDepartment/deptmaths.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideDepartment/deptmaths.dart
@@ -71,29 +71,6 @@ class DeptMaths extends StatelessWidget {
                 ),
               ),
             ),
-            /*  Card(
-              margin: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => TimeTable()));
-                },
-                splashColor: Colors.lightBlueAccent,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(
-                        Icons.access_time,
-                        size: 50.0,
-                      ),
-                      Text("Student's Time Table",
-                          style: new TextStyle(fontSize: 17.0))
-                    ],
-                  ),
-                ),
-              ),
-            ),*/
             Card(
               margin: EdgeInsets.all(8.0),
               child: InkWell(
@@ -532,81 +509,7 @@ class DeptMathsAboutp1 extends StatelessWidget {
   }
 }
 //End Of About Class
-//class time table
-/*
-class TimeTable extends StatefulWidget {
-  _TimeTable createState() => new _TimeTable();
-}
 
-class _TimeTable extends State<TimeTable> {
-  StreamSubscription<QuerySnapshot> subscription;
-  List<DocumentSnapshot> timeTable;
-
-  final CollectionReference collectionReference =
-      Firestore.instance.collection("MATHSTimeTable");
-
-  @override
-  void initState() {
-    super.initState();
-    subscription = collectionReference.snapshots().listen((datasnapshot) {
-      setState(() {
-        timeTable = datasnapshot.documents;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    subscription?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.blue[50],
-        appBar: AppBar(
-          title: Text(
-            'Dept. of Maths & Humanities', 
-          ),
-          backgroundColor: Colors.blue,
-          elevation: 10.0,
-        ),
-        body: timeTable != null
-            ? new StaggeredGridView.countBuilder(
-                padding: const EdgeInsets.all(9.0),
-                crossAxisCount: 1,
-                itemCount: timeTable.length,
-                itemBuilder: (context, i) {
-                  String imgPath = timeTable[i].data['url'];
-                  return new Material(
-                      elevation: 10.0,
-                      borderRadius:
-                          new BorderRadius.all(new Radius.circular(50.0)),
-                      child: new InkWell(
-                        onTap: () => Navigator.push(
-                            context,
-                            new MaterialPageRoute(
-                                builder: (context) =>
-                                    new FullScreenImagePage(imgPath))),
-                        child: Hero(
-                          tag: imgPath,
-                          child: new FadeInImage(
-                            placeholder: new AssetImage("assets/sdmlogo.jpg"),
-                            image: new NetworkImage(imgPath),
-                          ),
-                        ),
-                      ));
-                },
-                staggeredTileBuilder: (i) => new StaggeredTile.count(1, 1),
-                mainAxisSpacing: 2.0,
-                crossAxisSpacing: 2.0,
-              )
-            : new Loading());
-  }
-}*/
-
-//end of class TimeTable
 //start of faculty
 class FacultyDetails extends StatefulWidget {
   _FacultyDetails createState() => new _FacultyDetails();
