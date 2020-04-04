18a38ba88aebcf63847aee2da3411f811b3025ec
ganeshShetty98 <gs00619@gmail.com>
2020-04-04 22:16:43 +0530

    Update department.dart

--- sdmcetassist/lib/screens/home/insidehome/department.dart
+++ sdmcetassist/lib/screens/home/insidehome/department.dart
@@ -24,7 +24,7 @@ class Department extends StatelessWidget {
         elevation: 10.0,
       ),
       body: Container(
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(10.0),
         child: ListView(
           // crossAxisCount: 3,
           children: <Widget>[
