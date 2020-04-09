132ba57017bd47e8cd7637b2fc4198ab3515f0c7
Shubhamkumar6458 <shubhamkumar6458@gmail.com>
2020-04-09 11:06:37 +0530

    Update PlaceScreen1.dart

--- sdmcetassist/lib/screens/home/insidehome/insideTransport/PlaceScreen1.dart
+++ sdmcetassist/lib/screens/home/insidehome/insideTransport/PlaceScreen1.dart
@@ -129,7 +129,7 @@ class _AllRoute extends State<AllRoute> {
   List<DocumentSnapshot> timeTable;
 
   final CollectionReference collectionReference =
-      Firestore.instance.collection("CSEFacultyDetails");
+      Firestore.instance.collection("AfterNoonSession1:30");
 
   @override
   void initState() {
